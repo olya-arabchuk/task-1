40f2e73a13ab2e4008cf39074e9485b0990c6378
olya-arabchuk <olya.arabchuk@gmail.com>
2021-10-27 11:47:45 +0300

    Added dockerfile and docker-compose.yaml

--- src/main/java/com/example/demo/controller/HomepageController.java
+++ src/main/java/com/example/demo/controller/HomepageController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.persistance.Plain;
 import com.example.demo.service.PlainService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +15,15 @@ import java.util.List;
 @RestController
 public class HomepageController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomepageController.class);
+
     @Autowired
     private PlainService plainService;
 
     @GetMapping()
     public ResponseEntity<List<Plain>> getHomepage() {
         List<Plain> plains = plainService.getAllPlains();
+        LOGGER.info("Returning all data from db");
         return new ResponseEntity<>(plains, HttpStatus.OK);
     }
 }
